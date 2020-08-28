3bddf46ff7a61d47917e6cbd42aee0b3c39cdc77
amie-affection <amie.affection@gmail.com>
2020-08-29 02:07:20 +0300

    added contacts.js

---
+++ contacts.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const contactsPath = path.join(__dirname, './db/contacts.json');
+console.log(contactsPath);
+
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
